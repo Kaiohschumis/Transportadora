e8a5a69a9f0ea4c11b02cbd52f7affd68f676974
Kaiohschumis <Kaiohschumis@gmail.com>
2018-05-25 10:53:23 -0300

    Menu Principla
    ..

--- Entra21Transportadora/src/entra21/pkg/menu/MenuPrincipal.java
+++ Entra21Transportadora/src/entra21/pkg/menu/MenuPrincipal.java
@@ -2,8 +2,17 @@ package entra21.pkg.menu;
 
 import javax.swing.JOptionPane;
 
+import entra21.pkg.Carga;
+import entra21.pkg.Empresa;
+import entra21.pkg.Encomenda;
+import entra21.pkg.Transporte;
+
 public class MenuPrincipal {
 	MenuFuncionario menuFuncionario = new MenuFuncionario();
+	Transporte transportee = new Transporte();
+	Encomenda encomenda = new Encomenda(); 
+	Carga carga = new Carga();
+	Empresa empresa = new Empresa();
 	
 	public void abreMenu() {
 		boolean valida = false;
@@ -13,12 +22,23 @@ public class MenuPrincipal {
 				int menu = Integer.parseInt(JOptionPane.showInputDialog(null,
 					"*** MENU PRINCIPAL ***\n\n" 
 					+ "[1] FUNCIONARIO\n"
+					+ "[2] TRANSPORTE\n"
+				    + "[3] ENCOMENDA\n"
+					+ "[4] CARGA\n"
+				    + "[5] EMPRESA\n"
 					+ "[0] ENCERRAR"));
 				
 				switch (menu) {
 				case 1:
 					menuFuncionario.funcionario();
-					break;
+				case 2:
+					 transportee.cadastroTransporte();
+				case 3:
+					 encomenda.cadastroEncomeda();
+				case 4:
+					 carga.cadastroCarga();
+				case 5:
+					 empresa.cadastroEmpresa();
 				case 0:
 					valida = true;
 					break;
